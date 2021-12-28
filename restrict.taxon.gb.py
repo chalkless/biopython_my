39feace890f65412c2b618bd17d012eacba33ec7
chalkless <nakazato@dbcls.rois.ac.jp>
2021-12-28 15:23:40 +0900

    Add: restrict.taxon.gb.py: デバッグ（詳細表示）のため、-dをつけると処理するGenBank IDが表示されるようにした

--- restrict.taxon.gb.py
+++ restrict.taxon.gb.py
@@ -12,14 +12,18 @@ debug = 1
 parser = argparse.ArgumentParser()
 parser.add_argument('--file_in', '-f')
 parser.add_argument('--taxon', '-t')
+parser.add_argument('--detail_each', '-d', action="store_true")
 args = parser.parse_args()
 
 file_gb_in = args.file_in
 taxon_search = args.taxon
+option_each = args.detail_each
 
 with open(file_gb_in) as handle:
     for record in GenBank.parse(handle):
-#        print(inspect.getmembers(record))
+        print(inspect.getmembers(record))
+        if option_each:
+            print(record.accession, file=sys.stderr)
         taxonomy = record.taxonomy
         if taxon_search in taxonomy:
             print(record)
