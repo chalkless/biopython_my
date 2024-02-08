cb431f804eaca5faaa6ae2c9022a812406e2baa0
Takeru Nakazato <nakazato.tkr@gmail.com>
2024-02-08 18:01:43 +0900

    Update parseGenBank.py

--- parseGenBank.py
+++ parseGenBank.py
@@ -107,6 +107,8 @@ for record in SeqIO.parse(gb_file, 'genbank'):
                     voucherID = "|".join(vals)
                 elif type == 'gene' and k == 'gene':
                     gene = "|".join(vals)
+                elif k == 'product':
+                    product = "|".join(vals)
                 elif k == 'note':
                     note = "|".join(vals)
                             
